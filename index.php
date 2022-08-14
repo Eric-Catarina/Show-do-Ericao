011b5a0c047398fef64eeb07ef2d34c929ff742a
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-14 13:02:52 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+$nomeRecebido = $_GET["nome"]
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>htmlLá</title>
+</head>
+<body>
+    <h1>teste</h1>
+    <?php
+    echo $nomeRecebido
+    ?>
+</body>
+</html>
